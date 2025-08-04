20f18022e0710ffef351c326c917072749543b29
Liudmila Sukhadolskaya <liudmila.sukhadolskaya@gmail.com>
2025-08-04 20:00:13 +0200

    add tests and methods

---
+++ src/main/java/by/itacademy/sukhadolskaya/ICalculator.java
@@ -0,0 +1,8 @@
+package by.itacademy.sukhadolskaya;
+
+public interface ICalculator {
+    int sum(int a, int b);
+    int multiply(int a, int b);
+    double divide(int a, int b);
+    int subtract(int a, int b);
+}
